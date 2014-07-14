f375c300075296e610287cc173dd02a785f19a6d
Thierry Guichon <guichon.thierry@orbcomm.com>
2014-07-14 15:35:17 +0000

    Sampling task is working

---
+++ task_sampling.cpp
@@ -0,0 +1,141 @@
+
+#include "task_sampling.h"
+#include "/usr/include/uhd/usrp/multi_usrp.hpp"
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <csignal>
+#include <fstream>
+#include <cmath>
+#include "uhd_utilities.h"
+#include <pthread.h>
+
+
+/***********************************************************************//**
+Constructor: Creates the resources required for the task
+
+
+***************************************************************************/
+
+task_sampling::task_sampling(uhd::usrp::multi_usrp::sptr & usrp_ref)
+:usrp(usrp_ref), exit_task(false), current_buf(0)
+{
+	// Open the log file for the metadata
+	rx_log.open("rx_log.txt", std::ofstream::out);
+	if(rx_log.fail())
+	{
+		std::cout << " Metadata file could not be opened" << std::endl;
+		exit(1);
+	}
+	// Create a file to log the samples
+	rx_data.open("rx_data.txt", std::ofstream::out);
+	if(rx_data.fail())
+	{
+		std::cout << " Sample file could not be opened" << std::endl;
+		exit(2);
+	}
+
+
+}
+
+
+/***********************************************************************//**
+Destructor: Deallocate the resources required for the task
+
+
+***************************************************************************/
+
+task_sampling::~task_sampling()
+{
+	rx_log.close();
+	rx_data.close();
+
+}
+
+
+
+/***********************************************************************//**
+Starts the new thread
+
+@return true if an error occurred, false otherwise
+
+
+***************************************************************************/
+
+bool task_sampling::start()
+	{
+	 
+	// Create a streamer object - This defines the size of the samples
+	uhd::stream_args_t rx_stream_args("sc16", "sc16");
+	rx_stream = usrp->get_rx_stream(rx_stream_args);
+	
+	// Create the thread atttributes
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	size_t stack_size;
+	pthread_attr_getstacksize(&attr, &stack_size);
+	std::cout << "Stack Size: " << stack_size << std::endl;	
+			
+	// Start the thread
+	exit_task = false;
+	current_buf = 0;
+	int res = pthread_create (&thread_id, &attr, &task_sampling::helper, this);
+	if(res)
+	{
+		// an error in creating the thread occurred
+		// A message is displayed and the process aborts.
+		std::cout << "Thread for sampling_task could not be created" << std::endl;
+		return true;
+	}
+	return false;				
+}
+
+
+/***********************************************************************//**
+Main function of the RX sampling task. This is the function which effectively
+runs in a different thread.
+
+
+
+***************************************************************************/
+void * task_sampling::run()
+{
+
+	using namespace uhd;	
+	namespace radio = uhd::usrp;	
+
+	std::cout << "Inside thread id  " << thread_id << std::endl;	
+
+	// Structure to store the metadata of each received buffer
+	rx_metadata_t md;
+	// Send the command to start receiving data	
+	stream_cmd_t stream_cmd(stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+	stream_cmd.num_samps = g_inbuf[0].size();
+	stream_cmd.stream_now = true;
+	stream_cmd.time_spec = time_spec_t();
+	usrp->issue_stream_cmd(stream_cmd);
+
+	current_buf = 0;
+	// Infinite loop which fills each buffer alternatively
+	size_t rx_num;
+	while(!exit_task)
+	{
+		// Get the samples
+		size_t buf_size = g_inbuf[current_buf].size();		
+		rx_num = rx_stream->recv(&g_inbuf[current_buf].front(), buf_size, md, 5,false);
+		
+		// We write the info to the log file
+		rx_log << std::endl;
+		rx_log << "Samples Received: " << rx_num <<std::endl;
+		display_rx_metadata(md, rx_log);
+
+		// We write the data to the binary data file in an unformatted way
+		// Fromat of data is I16Q16I16Q16....
+		rx_data.write(reinterpret_cast<const char*>(&(g_inbuf[current_buf].front())), g_inbuf[current_buf].size()*sizeof(input_buf_t::value_type)); 
+		
+		// Switch buffer at each iteration
+		current_buf ^= 1;
+	}
+	
+	return NULL;
+}
---
+++ task_sampling.h
@@ -0,0 +1,67 @@
+/***********************************************************************//**
+@file
+
+Declaration ot type required to perform the sampling of data
+
+
+***************************************************************************/
+
+#ifndef TASK_SAMPLING_H
+#define TASK_SAMPLING_H
+
+#include <vector>
+#include <complex>
+#include "/usr/include/uhd/usrp/multi_usrp.hpp"
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+#ifdef DEFINE_GLOBALS
+	#define EXTERN
+#else
+	#define EXTERN extern
+#endif	
+
+
+typedef short sampling_type ; // samples are 16 bits signed I and Q
+typedef std::vector<std::complex<sampling_type> > input_buf_t;
+
+// Buffers to store the input samples in a ping-pong fashion
+EXTERN input_buf_t g_inbuf[2];
+
+
+
+/***********************************************************************//**
+This class represents the task which is running the sampling of the
+data and filling the buffers
+
+This class is a singleton
+
+***************************************************************************/
+class task_sampling
+{
+public:
+	task_sampling(uhd::usrp::multi_usrp::sptr & usrp);
+	bool start();
+	void stop() { exit_task = true;}
+	/// Returns a reference to the buffer which can be processed by other tasks
+	input_buf_t &get_buffer() {current_buf == 0? g_inbuf[1]:g_inbuf[0];}
+	/// Returns the  thread identifier
+	pthread_t get_tid() {return thread_id;}
+	~task_sampling();
+
+private:
+	static void * helper(void * arg) {return static_cast<task_sampling*>(arg)->run();}
+	uhd::usrp::multi_usrp::sptr & usrp;/// Hardware interface
+	uhd::rx_streamer::sptr rx_stream;  /// rx_streamer object to control the stream
+	void * run();			/// Main routine of the task
+	std::ofstream  rx_log;		/// ostream to write the metadata associated with each buffer
+	std::ofstream  rx_data;		/// osstream to write the sample data
+	pthread_t thread_id;	/// ID of the thread
+	bool exit_task;		/// Set to true to stop the task
+	int current_buf;		/// Buffer curretnly filled by the task
+	
+};
+
+
+#endif
